714217848097c772305a092cd10365a036cc5a64
Sandeepan Chakraborty <sandeepan.c@ad.infosys.com>
2018-07-13 14:47:29 +0530

    working on forecasts

--- src/Containers/Forecast.js
+++ src/Containers/Forecast.js
@@ -1,14 +1,74 @@
 import React, { Component } from 'react';
-import { Row, Col } from 'antd';
+import { Row, Col, Input } from 'antd';
+import { connect } from 'react-redux';
+import { fetchForecast } from '../actions/forecastActions';
 
 class Forecast extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            forecastData: null
+        };
+    }
+
+    componentDidMount() {
+        this.props.fetchForecast(this.props.forecast.city)
+    }
+
+    componentWillReceiveProps(nextProps) {
+        let data = nextProps.forecast && nextProps.forecast.forecastData;
+        if (data && data.cnt > 0 && Array.isArray(data.list)) {
+            let newForecastData = {};
+            data.list.forEach((el) => {
+                let dtTime = el.dt_txt.split(' ');
+                newForecastData[dtTime[0]] ? 
+                    newForecastData[dtTime[0]][dtTime[1]] = el : 
+                    newForecastData[dtTime[0]] = {
+                        [dtTime[1]]: el
+                    }
+            });
+            this.setState(() => ({
+                ...this.state,
+                forecastData: newForecastData
+            }))
+        }
+
+    }
+
+    componentDidUpdate(){
+        console.log(this.state.forecastData);
+    }
+
+    fetchForecastByCityname(value) {
+        this.props.fetchForecast(this.props.forecast.city, {
+            findBy: 'name',
+            value: value
+        });
+    }
+
     render() {
         return (
-            <Row>
-                <Col span={24}>Forecast</Col>
+            <Row style={{ padding: '1rem' }} gutter={24}>
+                <Col span={7}>
+                    <Input.Search style={{ width: '100%' }} placeholder="Search city" onSearch={value => this.fetchForecastByCityname(value)} enterButton />
+                </Col>
             </Row>
         );
     }
 }
 
-export default Forecast;
+const mapStateToProps = (state) => {
+    return {
+        forecast: state.forecast
+    }
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        fetchForecast: (city, options) => {
+            dispatch(fetchForecast(city, options))
+        }
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Forecast);
